91c3c12ee4ee1a358d333a80d670d5eda25bdf46
Noe Sola <88618857+Moyrasc@users.noreply.github.com>
2022-08-30 20:02:24 +0200

    correction exercise

--- exercises/02.3-loop-from-the-half-to-the-end/app.py
+++ exercises/02.3-loop-from-the-half-to-the-end/app.py
@@ -1,9 +1,9 @@
 my_list = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12]
 
 #Your code here:
-inicialValue = 7
-stopValue = 14
-increaseValue = 0
+inicialValue = len(my_list) // 2
+stopValue = my_list [-1]
+increaseValue = 1
 
 for i in range(inicialValue, stopValue):
     if i == my_list[i]:
